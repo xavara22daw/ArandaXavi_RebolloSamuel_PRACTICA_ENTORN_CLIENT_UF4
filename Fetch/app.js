241b4c13efa7bde849c771d2f9493db87acb9fc6
xavara22daw <xavara22daw@proton.me>
2023-02-16 16:32:01 +0100

    Avanzar Fetch

--- Fetch/app.js
+++ Fetch/app.js
@@ -1,3 +1,5 @@
+const contenedorComics = document.getElementById("resultados");
+const contenedorPersonajes = document.getElementById("personajes");
 
 async function cridaRemota(url = '', cos = {}) {
     // Les opciones per defecte estan marcades amb *
@@ -24,17 +26,43 @@ async function cridaRemota(url = '', cos = {}) {
   document.getElementById("entrada").onchange = function() {
     cridaRemota(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${this.value}&ts=1&apikey=3c043a9e457ce749d34745ac17502e1f&hash=197925ff06c90e7929be51c9028a1939` )
     .then(dades => {
-  
+      contenedorPersonajes.innerHTML = "";
       console.table(dades); 
-      document.getElementById("nom").innerHTML = dades.data.results[0].name;
+      for (let i = 0; i < dades.data.results.length; i++){
+        const personaje = dades.data.results[i].name;
+        const nuevoDiv = document.createElement("div");
+        nuevoDiv.textContent = personaje;
+        contenedorPersonajes.appendChild(nuevoDiv);
+      }
+      //document.getElementById("nom").innerHTML = dades.data.results[0].name;
 
       let idCharacter = dades.data.results[0].id;
 
-      cridaRemota(`https://gateway.marvel.com:443/v1/public/characters/${idCharacter}/comics?ts=1&apikey=3c043a9e457ce749d34745ac17502e1f&hash=197925ff06c90e7929be51c9028a1939`)
+      cridaRemota(`https://gateway.marvel.com:443/v1/public/characters/${idCharacter}/comics?ts=1&apikey=3c043a9e457ce749d34745ac17502e1f&hash=197925ff06c90e7929be51c9028a1939&limit=70`)
     .then(dades => {
-      console.log(dades.data.results[0].title);
-      document.getElementById("titulo").innerHTML = dades.data.results[0].title;
+      contenedorComics.innerHTML = "";
+      console.log(dades.data.results[0]);
+      const comic = dades.data.results[0];
 
+      const titulo = comic.title;
+      const descripcion = comic.description;
+      const thumbnail = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+      const creators = comic.creators.items;
+      const creador1 = creators[0].name;
+      const role1 = creators[0].role;
+      
+
+      console.log(titulo);
+      console.log(descripcion);
+      console.log(thumbnail);
+      console.log(creador1);
+      console.log(role1);
+      /*for (let i = 0; i < dades.data.results.length; i++){
+        const comic = dades.data.results[i].title;
+        const nuevoDiv = document.createElement("div");
+        nuevoDiv.textContent = comic;
+        contenedorComics.appendChild(nuevoDiv);
+      }*/
     })});
 
   };
